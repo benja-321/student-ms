5d1b9685c2c36e03199cda0f9b4c86d07ca6e2ee
benja-321 <became012@gmail.com>
2021-05-31 10:44:01 -0500

    Mejora de la documentacion de swagger

--- src/main/java/com/example/student/controller/StudentController.java
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.example.student.dto.RequestDto;
 import com.example.student.dto.ResponseDto;
 import com.example.student.mapper.StudentMapper;
 import com.example.student.service.StudentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api(tags = "Student")
 public class StudentController {
 
     @Autowired
@@ -21,26 +24,31 @@ public class StudentController {
     StudentMapper studentMapper;
 
     @GetMapping("/students")
+    @ApiOperation(value = "Return all students", notes = "Service that return all students saved")
     public List<ResponseDto> getAllStudents(){
         return studentMapper.mapIterableToListResponseDto(studentService.getAll());
     }
 
     @GetMapping("/student/{id}")
+    @ApiOperation(value = "Return a student by id", notes = "Service that return a student based on their id")
     public ResponseDto getStudentById(@PathVariable("id") Long id){
         return studentMapper.mapEntityToResponseDto(studentService.getById(id));
     }
 
     @PostMapping("/student")
+    @ApiOperation(value = "Save a student", notes = "Service that save a student and then return it")
     public ResponseDto insertStudent(@RequestBody RequestDto request){
         return studentMapper.mapEntityToResponseDto(studentService.save(studentMapper.mapRequestDtoToEntity(request)));
     }
 
     @PutMapping("/student/{id}")
+    @ApiOperation(value = "Update a student", notes = "Service that update a student and then return it")
     public ResponseDto updateStudent(@RequestBody RequestDto request, @PathVariable("id") Long id){
         return studentMapper.mapEntityToResponseDto(studentService.save(studentMapper.mapUpdateEntity(request,studentService.getById(id))));
     }
 
     @DeleteMapping("/student/{id}")
+    @ApiOperation(value = "Delete a student", notes = "Service that delete a student")
     public ResponseEntity<String> deleteStudent(@PathVariable("id") Long id){
         studentService.delete(id);
         return new ResponseEntity<>("Exito", HttpStatus.OK);
